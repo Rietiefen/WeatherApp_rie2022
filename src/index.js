dfe470223eb4780c31f2c0136e2617689a2fe896
Rietiefen <115991935+Rietiefen@users.noreply.github.com>
2022-10-20 10:55:27 +0200

    col changed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// Feature #1
+// CurrentTime&Date
 function updateTime() {
   let now = new Date();
   let hour = now.getHours();
@@ -32,17 +32,6 @@ function updateDate() {
 }
 updateDate();
 
-// // Feature #2
-// function changeCityName(event) {
-//   event.preventDefault();
-//   let inputCityName = document.getElementById("inputacity");
-//   let cityname = document.querySelector(".cityname");
-//   cityname.innerHTML = inputCityName.value;
-// }
-
-// let inputCityName = document.querySelector("button");
-// inputCityName.addEventListener("click", changeCityName);
-
 // // Feature #3
 // function mouseDownF() {
 //   document.getElementById("mainTemp").innerHTML = "90";
@@ -65,18 +54,6 @@ updateDate();
 
 // let tempC = document.querySelector(".tempC");
 // tempC.addEventListener("mousedown", mouseDownC);
-
-// display city Name
-// function insertCityName(event) {
-//   event.preventDefault();
-//   let cityName = document.getElementById("inputacity");
-//   let tempElement = document.querySelector(".cityname");
-
-//   // tempElement.innerHTML = cityName.value;
-// }
-
-// let cityName = document.querySelector("button");
-// cityName.addEventListener("click", insertCityName);
 
 // update searched City data
 function updateCityElement(response) {
@@ -134,7 +111,6 @@ function showCurrentTempPlace(response) {
 }
 
 function getCurrentPlace(position) {
-  // let apiKey = "10bffd0ee69586cf4e1b1e702883c72c";
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=10bffd0ee69586cf4e1b1e702883c72c`;
